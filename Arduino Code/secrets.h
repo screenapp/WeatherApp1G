36df2359bb2154c60e82e3a35f4ce7ce5a93572a
screenapp <82409052+screenapp@users.noreply.github.com>
2021-04-14 18:41:58 -0400

    Create secrets.h
    Adds support for UV sensor

---
+++ Arduino Code/secrets.h
@@ -0,0 +1,14 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
+
+/*Orignal ThinkSpeak Channel that was for both sensors. Now only for Temp and Humidity*/
+//#define SECRET_CH_ID 1292014			               // replace 0000000 with your channel number
+//#define SECRET_WRITE_APIKEY "8JM99WUHS6RF1FD6"   // replace XYZ with your channel write API Key
+
+
+/*NEW ThingSpeak Channel for UV Sensor*/
+#define SECRET_CH_ID 1323243                     // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "N8U0LOVIZHFNNGFN"   // replace XYZ with your channel write API Key
